bbe763b91ebdd6fbd859dd4004c81c8eb1e8b04a
Adolfo Gonzalez <agonzal177@gmail.com>
2022-01-28 22:45:13 -0500

    added link to reset.css and made my works cards go to deployed github pages

---
+++ script.js
@@ -0,0 +1,3 @@
+const project1=()=>window.location.href="https://adolfo-g.github.io/Countdown-Games/"
+const weather=()=>window.location.href="https://adolfo-g.github.io/Weather-Forecast/"
+const schedule=()=>window.location.href="https://adolfo-g.github.io/Work-Day-Scheduler/"
