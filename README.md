6bd0990f169bd5f9d070eeacdc9a72860a224a3d
Adolfo Gonzalez <agonzal177@gmail.com>
2022-03-25 22:47:54 -0400

    readme and portfolio content update

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
-# Portfolio
-The following is a portfolio page which displays an about me, a few sampled works, and contact information. There is also a downloadable copy of my resume and links to both linked in and github.The page was built using the foundation framework.
-This protfolio is deployed here: https://adolfo-g.github.io/Portfolio/
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  # My Portfolio
+  ## Description
+
+  The following is a portfolio page which displays an about me, a few sampled works, and contact information. There is also a downloadable copy of my resume and links to both linked in and github.The page was built using the foundation framework.
+
 ![](assets/images/preview.jpeg)
+
+  ## Table of Contents
+  * [Usage](#usage)
+  * [License](#license)
+  * [Questions](#questions)
+
+  ## Usage
+  The deployed page can be viewed here: https://adolfo-g.github.io/Portfolio/
+ 
+  ## License
+ 
+  https://opensource.org/licenses/MIT
+
+  ## Questions
+  -For any questions or concerns please reach out to me at
+    github:  https://github.com/Adolfo-G
+    email:  adolfoG5545@gmail.com
+    
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+const card1LinkBtn = document.querySelector('#card1')
+const card2LinkBtn = document.querySelector('#card2')
+const card3LinkBtn = document.querySelector('#card3')
+
+const card1GithubBtn = document.querySelector('#gh1')
+const card2GithubBtn = document.querySelector('#gh2')
+const card3GithubBtn = document.querySelector('#gh3')
+
+const project2=()=>window.location.href="https://mortal-instruments-music-shop.herokuapp.com/"
+const project2Github=()=>window.location.href="https://github.com/smeske10/Electric-Wipeouts-Mortal-Instruments"
+
 const project1=()=>window.location.href="https://adolfo-g.github.io/Countdown-Games/"
+const project1Github=()=>window.location.href="https://github.com/Adolfo-G/Countdown-Games"
+
 const weather=()=>window.location.href="https://adolfo-g.github.io/Weather-Forecast/"
-const schedule=()=>window.location.href="https://adolfo-g.github.io/Work-Day-Scheduler/"
+const weatherGithub=()=>window.location.href="https://github.com/Adolfo-G/Weather-Forecast"
+
+card1LinkBtn.addEventListener('click',project2)
+card2LinkBtn.addEventListener('click',project1)
+card3LinkBtn.addEventListener('click',weather)
+
+card1GithubBtn.addEventListener('click',project2Github)
+card2GithubBtn.addEventListener('click',project1Github)
+card3GithubBtn.addEventListener('click',weatherGithub)
